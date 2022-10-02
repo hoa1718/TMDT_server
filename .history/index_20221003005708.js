661b5d9ef77c28ca1af9d77210c171215d961de2
gia hoa <nhamgiahoa1806@gmail.com>
2022-10-03 01:08:33 +0700

    second

--- .history/index_20221003005708.js
+++ .history/index_20221003005708.js
@@ -1,13 +1,13 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const morgan = require('morgan')
 app.use(morgan('combined'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT||port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
