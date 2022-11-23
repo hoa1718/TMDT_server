4bc26b0e46c69842fb9ab86ed900369f1ff56404
hoa1718 <82604780+hoa1718@users.noreply.github.com>
2022-11-24 01:28:01 +0700

    Update Phim.js

--- routes/Phim.js
+++ routes/Phim.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller= require("../controller/phim");
+const controller= require("../controller/Phim");
 
 router.get('/', controller.getPhim)
 router.get('/PhimAdmin', controller.getPhimAdmin)
