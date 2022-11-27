db198e49625531abb433bd59cfc39cdc1b484cbc
hoa1718 <82604780+hoa1718@users.noreply.github.com>
2022-11-27 23:25:23 +0700

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,21 +2,20 @@ function connected(){
     var sql = require("mssql");
   
     // config for your database
-    var config = {
-        user: 'admin',
-        password: '12345',
-        server: 'localhost', 
-        database: 'TMDT',
-        trustServerCertificate: true
-    };
     // var config = {
-    //     user: 'db_a90403_admin_admin',
-    //     password: 'Giahoa123',
-    //     server: 'sql8001.site4now.net', 
-    //     database: 'db_a90403_admin',
+    //     user: 'admin',
+    //     password: '12345',
+    //     server: 'localhost', 
+    //     database: 'TMDT',
     //     trustServerCertificate: true
     // };
-    // connect to your database
+    var config = {
+        user: 'db_a90403_admin_admin',
+        password: 'Giahoa123',
+        server: 'sql8001.site4now.net', 
+        database: 'db_a90403_admin',
+        trustServerCertificate: true
+    };
     sql.connect(config, function (err) {
     
         if (err) console.log(err);
