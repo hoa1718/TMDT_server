134992fe17cb572d0dcc793cff930a4614e554a4
hoa1718 <82604780+hoa1718@users.noreply.github.com>
2022-11-29 12:50:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ db.connected();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'img')));
-app.use(function(req, res, next) {
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-     next();
-});
+// app.use(function(req, res, next) {
+//      res.header("Access-Control-Allow-Origin", "*");
+//      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//      next();
+// });
 
 const morgan = require('morgan');
 app.use(morgan('combined'))
 
 require('dotenv').config();
 
-const whitelist = ['https://tmdt-21-server.herokuapp.com/','https://tmdt-admin.herokuapp.com/','https://tmdt-client.herokuapp.com'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+// const whitelist = ['https://tmdt-21-server.herokuapp.com/','https://tmdt-admin.herokuapp.com/','https://tmdt-client.herokuapp.com'];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions));
 // app.options('*', cors())
-// app.use(cors());
+app.use(cors());
 router(app)
 
 app.get('/', (req, res) => {
