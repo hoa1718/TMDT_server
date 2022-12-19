d2d47af829b02d7d61ea2fbf757126f35973a272
gia hoa <nhamgiahoa1806@gmail.com>
2022-12-20 01:12:34 +0700

    a

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,21 +2,21 @@ function connected(){
     var sql = require("mssql");
   
     // config for your database
-    var config = {
-        user: 'hoa',
-        password: 'Giahoa123',
-        server: 'localhost', 
-        database: 'TMDT',
-        trustServerCertificate: true,
-        
-    };
     // var config = {
-    //     user: 'db_a90403_admin_admin',
+    //     user: 'hoa',
     //     password: 'Giahoa123',
-    //     server: 'sql8001.site4now.net', 
-    //     database: 'db_a90403_admin',
-    //     trustServerCertificate: true
+    //     server: 'localhost', 
+    //     database: 'TMDT',
+    //     trustServerCertificate: true,
+        
     // };
+    var config = {
+        user: 'db_a90403_admin_admin',
+        password: 'Giahoa123',
+        server: 'sql8001.site4now.net', 
+        database: 'db_a90403_admin',
+        trustServerCertificate: true
+    };
     // connect to your database
     sql.connect(config, function (err) {
     
